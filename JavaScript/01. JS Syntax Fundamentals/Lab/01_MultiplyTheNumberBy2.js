8dd607d96de9b52e128d311d746bd7523cf7d784
Kostadin Ivanov <k.ivanov.dceo@gmail.com>
2023-12-03 17:41:49 +0200

    Added JavaScript Module + Lab Tasks

---
+++ JavaScript/01. JS Syntax Fundamentals/Lab/01_MultiplyTheNumberBy2.js
@@ -0,0 +1,11 @@
+function multiplyNumBy2(num) {
+    let result = 0;
+    result = num * 2;
+    console.log(result);
+}
+// function solve(num){
+//     console.log(num*2);
+// }
+
+
+multiplyNumBy2(2);
